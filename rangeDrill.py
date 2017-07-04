2c635b3d36b83e2aca701282ff3213b74897bdf3
oliver fu <ofu997@gmail.com>
2017-07-03 18:54:26 -0700

    write drill

---
+++ rangeDrill.py
@@ -0,0 +1,27 @@
+'''
+1. Start IDLE and use the Python range() function with one parameter to display the
+following:
+0
+1
+2
+3
+When this is working show it to your instructor.
+2. Use the Python range() function with 3 parameters to display the following:
+3 2 1 0
+When this is working show it to your instructor.
+3. Use the Python range() function with 3 parameters to display the following:
+8 6 4 2
+When this is working show it to your instructor.
+'''
+
+print("with 1 parameter: \n")
+for i in range (4):
+  print(i)
+
+print("with 3 parameters: \n")
+for i in range(3,-1,-1):
+  print(i)
+
+print("8 6 4 2 with 3 parameters: \n")
+for i in range (8, 0, -2):
+  print(i)
